55aa488551323dfb633f8aa76f143e4877ff5283
Md. Zabaidul Islam <zislam5622@gmail.com>
2025-12-05 21:57:53 +0600

    add users database table

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,6 +6,18 @@ export const pool = new Pool({
   connectionString: `${config.connection_str}`,
 });
 
-const initDB = async () => {};
+const initDB = async () => {
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS Users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL CHECK (email = LOWER(email)),
+        password TEXT NOT NULL CHECK (LENGTH(password) >= 6),
+        phone VARCHAR(15) NOT NULL,
+        role VARCHAR(20) CHECK (role IN ('admin', 'customer'))
+        )
+        `);
+  console.log("Database Connected");
+};
 
 export default initDB;
