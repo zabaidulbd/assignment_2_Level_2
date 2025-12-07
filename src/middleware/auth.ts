7260128cbc63bd13f1786e162eac2cd3ff950857
Md. Zabaidul Islam <zislam5622@gmail.com>
2025-12-07 11:20:35 +0600

    add auth

---
+++ src/middleware/auth.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import config from "../config";
+
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const authHeader = req.headers.authorization;
+
+      if (!authHeader) {
+        return res.status(401).json({ message: "No token provided!" });
+      }
+
+      const token = authHeader.split(" ")[1];
+      if (!token) {
+        return res.status(500).json({ message: "You are not authorized!!" });
+      }
+      const decoded = jwt.verify(
+        token,
+        config.jwtSecret as string
+      ) as JwtPayload;
+      req.user = decoded;
+
+      if (roles.length && !roles.includes(decoded.role as string)) {
+        return res.status(500).json({
+          error: "you are not authorized!!!",
+        });
+      }
+
+      next();
+    } catch (err: any) {
+      res.status(500).json({
+        success: false,
+        message: err.message,
+      });
+    }
+  };
+};
+
+export default auth;
